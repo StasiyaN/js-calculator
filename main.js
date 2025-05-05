00ceada4b03a9501e3a11818f0d2f6676be13af8
StasiyaN <ana.nyzhalska@gmail.com>
2025-05-05 18:27:16 +0200

    js ok

---
+++ main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM полностью загружен и разобран");
+
+    let display = document.querySelector('.display');
+    let buttons = Array.from(document.querySelectorAll('button'));
+
+    buttons.map((button) => {
+        button.addEventListener('click', (e) => {
+            console.log(e.target.innerText);
+            switch (e.target.innerText) {
+                case "AC":
+                    display.innerText = "0";
+                    break;
+                case "=":
+                    try {
+                        display.innerText = eval(display.innerText);
+                    } catch (e) {
+                        display.innerText = "error";
+                    }
+                    break;
+                case "%":
+                    let passedText = display.innerText + "/100";
+                    display.innerText = eval(passedText);
+                    break;
+                case"+/-":
+                    display.innerText = "-";
+                    break;
+
+
+                default:
+                    if(display.innerText === "0" && e.target.innerText !== ".") {
+                        display.innerText = e.target.innerText;
+                    } else {
+                    display.innerText += e.target.innerText;
+                    }
+            }
+        });
+    });
+});
